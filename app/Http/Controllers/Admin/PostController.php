6727b04db91a5a55038919ae141df1cf8202d241
niculas1989 <nicolas.maranzano@libero.it>
2022-03-30 15:30:05 +0100

    added right choises for "destroy"

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -151,6 +151,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if (count($post->tags)) $post->tags()->detach();
         $post->delete();
         return redirect()->route('admin.posts.index')->with('message', "$post->title eliminato con successo.")->with('type', 'success');
     }
