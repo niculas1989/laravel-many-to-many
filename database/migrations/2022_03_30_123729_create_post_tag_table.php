494f4a2b3f3bffbf296c7619b94b8bb6bb6a9330
niculas1989 <nicolas.maranzano@libero.it>
2022-03-30 13:42:46 +0100

    added rules for new table

--- database/migrations/2022_03_30_123729_create_post_tag_table.php
+++ database/migrations/2022_03_30_123729_create_post_tag_table.php
@@ -15,6 +15,13 @@ class CreatePostTagTable extends Migration
     {
         Schema::create('post_tag', function (Blueprint $table) {
             $table->id();
+
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('tag_id');
+
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
